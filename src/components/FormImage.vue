99cf14e86cf4a6b2fd1a8813c72ef431287659f4
claupimentel <claupimentel98@hotmail.com>
2020-05-11 15:00:49 -0300

    criado componente com imagem para os forms iniciais

---
+++ src/components/FormImage.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="hero is-fullheight">
+      <div class="hero-body no-padding">
+        <div class="container">
+          <slot/>
+        </div>
+        <div class="container width">
+          <img class="image is-pulled-right" :src="background">
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+import BackgroundImage from '../img/background.jpg';
+
+export default {
+  data() {
+    return {
+      background: BackgroundImage,
+    };
+  },
+};
+
+</script>
+
+<style lang="scss">
+.no-padding {
+  padding: 0px !important;
+}
+
+.image {
+  max-height: 682px;
+}
+
+.width {
+  width: 20% !important;
+}
+</style>
