baf2810dd343534ee9c2a974f7677080a529e4dc
claupimentel <claupimentel98@hotmail.com>
2020-05-11 22:23:19 -0300

    inicio da tela de login

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form-image>
+      <validation-input
+        v-model="value"
+        title="Email"
+        placeholder="email@exemplo.com"
+        type="email"
+        class="margin"
+      />
+      <validation-input v-model="value" title="Senha" placeholder="senha" type="password"/>
+    </form-image>
+  </div>
+</template>
+
+<script>
+import FormImage from '../components/FormImage.vue';
+import ValidationInput from '../components/ValidationInput.vue';
+
+export default {
+  components: {
+    FormImage,
+    ValidationInput,
+  },
+  data() {
+    return {
+      value: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.margin {
+  margin-bottom: 20px;
+}
+</style>
