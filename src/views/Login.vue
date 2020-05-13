7322c1200be7aff1941ca36cef363bf028ddce64
claupimentel <claupimentel98@hotmail.com>
2020-05-12 21:41:43 -0300

    criação componente para header do form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
     <form-image>
-        <div class="margin-title-description">
-          <p class="big-title-style is-pulled-left">Entre</p>
-          <p class="description-style is-pulled-left">
-            Por favor, entre com suas credenciais para continuar
-          </p>
-        </div>
-        <validation-input
-          v-model="email"
-          title="Email"
-          placeholder="email@exemplo.com"
-          type="email"
-          class="margin-input"
-        />
-        <validation-input v-model="password" title="Senha" placeholder="senha" type="password">
-          <router-link class="label-style" to="/">Esqueceu sua senha?</router-link>
-        </validation-input>
-        <div class="centered">
-          <label class="label-style width-center">
-            Não tem uma conta? <router-link class="link" to="/">Cadastre-se</router-link>
-          </label>
-        </div>
-        <div class="centered-button">
-          <button class="button is-fullwidth">Entre</button>
-        </div>
+      <form-header title="Entre" subtitle="Por favor, entre com suas credenciais para continuar"/>
+      <validation-input
+        v-model="email"
+        title="Email"
+        placeholder="Insira seu e-mail"
+        type="email"
+        class="margin-input"
+      />
+      <validation-input
+        v-model="password"
+        title="Senha"
+        placeholder="Insira sua senha"
+        type="password"
+      >
+        <router-link class="label-style" to="/">Esqueceu sua senha?</router-link>
+      </validation-input>
+      <div class="centered">
+        <label class="label-style width-center">
+          Não tem uma conta? <router-link class="link" to="/">Cadastre-se</router-link>
+        </label>
+      </div>
+      <div class="centered-button">
+        <button class="button is-fullwidth">Entre</button>
+      </div>
     </form-image>
   </div>
 </template>
@@ -32,11 +32,13 @@
 <script>
 import FormImage from '../components/FormImage.vue';
 import ValidationInput from '../components/ValidationInput.vue';
+import FormHeader from '../components/FormHeader.vue';
 
 export default {
   components: {
     FormImage,
     ValidationInput,
+    FormHeader,
   },
   data() {
     return {
@@ -52,21 +54,18 @@ export default {
   margin-bottom: 20px;
 }
 
-.margin-title-description {
-  margin-bottom: 100px;
-}
-
 .link {
   color: #2E5BFF;
 }
 
 .centered {
   text-align: center;
+  margin-top: 10px;
 }
 
 .centered-button {
   margin: auto;
   width: 50%;
-  margin-top: 20px;
+  margin-top: 15px;
 }
 </style>
