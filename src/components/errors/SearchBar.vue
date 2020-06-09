17d0f15498605581166ccd5a7d718d91b3435342
claupimentel <claupimentel98@hotmail.com>
2020-06-09 11:06:03 -0300

    correção para seleção de arquivados

--- src/components/errors/SearchBar.vue
+++ src/components/errors/SearchBar.vue
@@ -187,6 +187,7 @@ export default {
     },
     getArchivedErrors() {
       this.dropdownOptions = false;
+      this.setSelectedItems([]);
       this.closeDropdown();
       this.isArchivedErrors = !this.isArchivedErrors;
       this.setParamsArchived(this.isArchivedErrors);
